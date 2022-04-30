0b0dd384e81c148b3fcd90fe2874d7c2c2835d28
NikitaPanasiuk <frutis2014@gmail.com>
2022-04-30 18:08:44 +0300

    задача на вывод алфавита

---
+++ ProjectSDK/src/main/java/Task3.java
@@ -0,0 +1,9 @@
+public class Task3 {
+    public static void main(String[] args) {
+        char x = 'a';
+        for (int i = 0; i < 26; i++) {
+            System.out.println(x);
+            x++;
+        }
+    }
+}
