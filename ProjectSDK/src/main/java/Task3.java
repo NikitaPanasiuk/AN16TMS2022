af62f331f4b6d8a0427e408d66742277d360a5e4
NikitaPanasiuk <104629145+NikitaPanasiuk@users.noreply.github.com>
2022-05-01 14:34:44 +0300

    Delete ProjectSDK directory

--- ProjectSDK/src/main/java/Task3.java
+++
@@ -1,9 +0,0 @@
-public class Task3 {
-    public static void main(String[] args) {
-        char x = 'a';
-        for (int i = 0; i < 26; i++) {
-            System.out.println(x);
-            x++;
-        }
-    }
-}
