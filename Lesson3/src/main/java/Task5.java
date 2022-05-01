5593950b58957baffbb6800a34b3e4f90511c1d8
NikitaPanasiuk <frutis2014@gmail.com>
2022-05-01 21:46:47 +0300

    домашка

--- Lesson3/src/main/java/Task5.java
+++ Lesson3/src/main/java/Task5.java
@@ -1,5 +1,16 @@
 public class Task5 {
     public static void main(String[] args){
-        System.out.println();
+        System.out.println("""
+                           byte\tshort\tchar\tint \tlong\tfloat\tdouble\tboolean
+                   byte     т     ня     я     ня    ня     я      я       х
+                   short    я      т     я     ня    ня     я      я       х
+                   char     я      я     т     ня    я      я      я       х
+                   int      я      я     я     т     ня     я      ня      х
+                   long     я      я     я     я     т      я      ня      х
+                   float    я      я     я     я     я      т      я       х\040\040
+                   double   я      я     я     я     я      я      т       х
+                   boolean  х      х     х     х     х      х      х       х
+                """);
+
     }
 }
