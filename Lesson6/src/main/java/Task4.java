fa135682e1e27b732a3c73872f57929375aee70e
NikitaPanasiuk <frutis2014@gmail.com>
2022-05-07 17:35:49 +0300

    исправлен поиск факториала

--- Lesson6/src/main/java/Task4.java
+++ Lesson6/src/main/java/Task4.java
@@ -1,3 +1,4 @@
+import java.math.BigInteger;
 import java.util.Scanner;
 
 //Найти n-ую строку Треугольника Паскаля и вывести ее на экран
@@ -6,10 +7,10 @@ public class Task4 {
         Scanner in = new Scanner(System.in);
         System.out.println("input number of string");
         int num = in.nextInt();
-        int[][] string = new int[num][num];
+        BigInteger[][] string = new BigInteger[num][num];
         for (int n = 0; n < num; n++) {
             for (int k = 0; k < num; k++) {
-                string[n][k] = getFactorial(n) / (getFactorial(k) * getFactorial((n - k)));
+                string[n][k] = getFactorial(n).divide((getFactorial(k).multiply(getFactorial((n - k)))));
             }
         }
         for (int j = 0; j < num; j++) {
@@ -17,11 +18,11 @@ public class Task4 {
         }
     }
 
-    public static int getFactorial(int f) {
-        int result = 1;
-        for (int i = 1; i <= f; i++) {
-            result = result * i;
-        }
+    public static BigInteger getFactorial(int f) {
+        BigInteger result = BigInteger.ONE;
+        for (int i = 1; i <= f; i++)
+            result = result.multiply(BigInteger.valueOf(i));
         return result;
     }
 }
+
