750b0fd66f282754b1fb5599e04a3cd81836dc55
NikitaPanasiuk <frutis2014@gmail.com>
2022-06-16 22:20:41 +0300

    дз11

---
+++ Lesson12/src/main/java/by/tms/task1/Main.java
@@ -0,0 +1,30 @@
+package by.tms.task1;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        StringBuilder s = new StringBuilder();
+        FileWriter fr = new FileWriter("C:\\TechMeSkill\\Java\\Task\\AN16TMS2022\\Lesson12\\src\\main\\java\\by\\tms\\task1\\output.txt");
+        Scanner in = new Scanner(new File("C:\\TechMeSkill\\Java\\Task\\AN16TMS2022\\Lesson12\\src\\main\\java\\by\\tms\\task1\\input.txt"));
+        while (in.hasNext())
+            s.append(in.nextLine());
+        // ------------------------------------
+        String[] a = s.toString().split("\\n"); //!!!!
+
+        for (String str : a) {
+            if (isPalindrome(str)) {
+                System.out.println(str);
+                fr.write(" " + str);
+            }
+        }
+        fr.close();
+    }
+
+    public static boolean isPalindrome(String str) {
+        return str.equalsIgnoreCase(new StringBuilder(str).reverse().toString());
+    }
+}
