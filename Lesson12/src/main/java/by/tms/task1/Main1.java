3cec680dba744b789f2438cb35ac374c5811c245
NikitaPanasiuk <frutis2014@gmail.com>
2022-06-16 23:27:44 +0300

    дз12

---
+++ Lesson12/src/main/java/by/tms/task1/Main1.java
@@ -0,0 +1,39 @@
+package by.tms.task1;
+
+import java.io.*;
+import java.util.ArrayList;
+
+public class Main1 {
+    public static void main(String[] args) throws IOException {
+        File file = new File("C:\\TechMeSkill\\Java\\Task\\AN16TMS2022\\Lesson12\\src\\main\\java\\by\\tms\\task1\\input.txt");
+        //создаем объект FileReader для объекта File
+        FileReader fr = new FileReader(file);
+        FileWriter fw = new FileWriter("C:\\TechMeSkill\\Java\\Task\\AN16TMS2022\\Lesson12\\src\\main\\java\\by\\tms\\task1\\output.txt");
+        //создаем BufferedReader с существующего FileReader для построчного считывания
+        BufferedReader reader = new BufferedReader(fr);
+        // считаем сначала первую строку
+        String line = reader.readLine();
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add(line);
+        while (line != null) {
+            line = reader.readLine();
+            arrayList.add(line);
+        }
+        for (String str : arrayList) {
+            if (isPalindrome(str)) {
+                System.out.println(str);
+                fw.write(" " + str);
+            }
+        }
+        fr.close();
+    }
+
+    public static boolean isPalindrome(String str) {
+        if (str != null) {
+            return str.equalsIgnoreCase(new StringBuilder(str).reverse().toString());
+        }
+        return false;
+    }
+
+}
+
